952318b3a0bf138cd69fafc12964f8133410ffa4
Apoorvap432 <apoorvapanwar488@gmail.com>
2025-11-26 17:16:47 +0530

    string slicing

---
+++ StringSlicing.py
@@ -0,0 +1,14 @@
+name = "Apoorva"
+
+slicedName = name[0:3]  #include alphabets from index 0-2, excluding 3
+
+print(slicedName)
+
+print(name[-5:-2])
+
+#left to right jaege to index 0 se suru hogi aur right to left jaege to index -1 se suru hogi
+
+
+three = '0123456789'
+threeSliced = three[0:9:3]
+print(threeSliced)
