f5c0986882b02a13d9a7320537b37d81f47f844e
Apoorvap432 <apoorvapanwar488@gmail.com>
2025-11-26 17:27:26 +0530

    string functions

---
+++ StringFunctions.py
@@ -0,0 +1,20 @@
+name = "apoorva Panwar"
+
+print("----length----")
+#for length
+print(len(name))
+
+print("----ends with----")
+#ends with
+print(name.endswith("war"))
+print(name.endswith("wa"))
+
+print("----starts with----")
+#startswith
+print(name.startswith("a"))
+print(name.startswith("A"))
+print(name.startswith("app"))
+
+print("----capitalize----")
+#capitalise
+print(name.capitalize())
--- StringSlicing.py
+++ StringSlicing.py
@@ -11,4 +11,6 @@ print(name[-5:-2])
 
 three = '0123456789'
 threeSliced = three[0:9:3]
+#upar vle mein suru k do vse hi kaam krege jse krte hai... third vli jagah pe jo 
+# hoga vo define krega k suru k 2 jo range dkha rhe h usme 3rd vle jitna gap de kr kya kya aega
 print(threeSliced)
