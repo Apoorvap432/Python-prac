e40d0409cb4a2f4b4b1ba54f763a644e5e213f25
Apoorvap432 <apoorvapanwar488@gmail.com>
2025-12-21 17:56:18 +0530

    pass explained in break and continue file

--- Break_and_continue.py
+++ Break_and_continue.py
@@ -19,3 +19,12 @@ for j in l:
     if j == "good":
         continue
     print(j)
+
+
+'''
+to use pass:
+
+Agar hm kisi loop pe kaam kr rhe h aur use bich m chd k hm koi or loop pe kaam krne lgte h to vo 
+incomplete loop error throw krega. Us error se bachne k liye hm us incomplete loop k baad pass 
+likh dega to vo function us loop k liye na ruk k use pass kr dega next loop ko
+'''
