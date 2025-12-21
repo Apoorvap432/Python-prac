383de6755a789bab1d40ca7cf1c33e77c72d0f46
Apoorvap432 <apoorvapanwar488@gmail.com>
2025-12-21 17:43:36 +0530

    for loop done

--- Loops.py
+++ Loops.py
@@ -6,3 +6,30 @@ for i in range(1, 12):
 '''another method'''
 for j in range(1, 70, 5):
     print(j)
+
+
+print("----tuple list-----")
+'''tuple'''
+t = (1, 24, 75)
+for i in t:
+    print (i)
+
+print("----list-----")
+li = [34, 43, 12, 21]
+for i in li:
+    print(i)
+
+
+print("----name characters----")
+name = 'Apoorva'
+for i in name:
+    print(i)
+
+
+print("--- with else----")
+i = 1
+
+for i in range(1, 4):
+    print (i)
+else:
+    print("no more nos left")
