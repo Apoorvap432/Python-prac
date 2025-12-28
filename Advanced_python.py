f0df4f9009d848113a17f85dac0e2636e5c955d9
Apoorvap432 <apoorvapanwar488@gmail.com>
2025-12-28 22:04:45 +0530

    some new things in python 3.8

--- Advanced_python.py
+++ Advanced_python.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple, Dict, Union
+
 #using walrus operator
 if(n := len([1, 2, 3, 4, 5])) > 3:
     print(f"Length is too long ({n} elements, expected <= 3)")
@@ -9,3 +11,18 @@ s : str = "Appu"
 
 def sum(a: int, b:int) -> int:
     return a + b
+
+#syntax of this is basically like this:
+
+#list of integers
+numbers: list[int] = [1, 2, 3, 4, 5]
+
+#tuple
+person : Tuple[str, int] = ("Appu", 420)
+
+#dictionary
+scores: Dict[str, int] = {"Appu": 90, "Apoorva": 99}
+
+#union
+identifier: Union[int, str] = "ID420"   #either int or str
+identifier: 12345
