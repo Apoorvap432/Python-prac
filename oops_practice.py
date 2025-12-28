068098c3893c579fedbd747233120d05ed97e8a1
Apoorvap432 <apoorvapanwar488@gmail.com>
2025-12-28 19:29:59 +0530

    oops part 1 practice set complete

--- oops_practice.py
+++ oops_practice.py
@@ -1,3 +1,5 @@
+from random import randint
+
 #class programmer with employee details
 
 class Programmer:
@@ -16,4 +18,48 @@ print(r.name, r.salary, r.pin, r.company)
 
 
 
-#
+#class calculator capable of finding square, cube, square root of a number. 
+class calculator:
+
+    def __init__(self, n):
+        self.n = n
+
+    def square(self):
+        print(f"The square is {self.n*self.n}")
+    def cube(self):
+        print(f"The square is {self.n*self.n*self.n}")
+    def squareroot(self):
+        print(f"The square is {self.n**1/2}")
+
+    #static method
+    @staticmethod
+    def greet():
+        print("Hello user")
+
+a = calculator(4)
+a.square()
+a.cube()
+a.squareroot()
+a.greet()
+
+
+# Add class train which has methods to book tickets, get status(no. of seats), and get fair info of train running under indian railways
+
+class train:
+
+    def __init__(self, trainNum):
+        self.trainNo = trainNum
+
+    def book(self, frm, to):
+        print(f"Train is booked from {frm} to {to}")
+
+    def status(self):
+        print(f"Train number {self.trainNo} is running 10 mins late.")
+
+    def fair(self, frm, to):
+        print(f"The fair for train {self.trainNo} from {frm} to {to} is {randint(225, 2000)}")\
+        
+a = train(35245)
+a.book("Muzaffarnagar", "Delhi")
+a.status()
+a.fair("Muzaffarnagar", "Delhi")
